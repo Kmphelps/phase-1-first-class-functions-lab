0251d62039cdb739bb8cbc5389cbe7de7393f44a
Kayla Phelps <kmphelps822@gmail.com>
2021-07-21 14:46:59 -0700

    commit tree f541dde8bc7140fb4fdbe85e89f22e2e8bb3f462

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function() {
+    const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+    return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(int) { 
+    return function fareMultiplier(fare) {
+        return fare * int;
+    }
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(arrayOfDrivers, driversFunction) {
+    return driversFunction();
+}
+selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+
+
+
+
+
+ 
+
+
+
